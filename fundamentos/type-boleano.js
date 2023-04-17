bbf8b911db233d649785a64e4c4316ce067d90fb
Elias <eliassiqueira584@gmail.com>
2023-04-17 20:06:49 -0300

    tipos primitivos number, boolean,string

---
+++ fundamentos/type-boleano.js
@@ -0,0 +1,13 @@
+//boolean
+//true -> verdadeiro
+//false -> falso
+
+const primeiroNumero = 15;
+const segundoNumero = 62;
+
+//console.log(primeiroNumero === segundoNumero);
+
+const texto1 = 'Elias';
+const texto2 = 'elias';
+
+console.log(texto1 === texto2);
---
+++ fundamentos/type-string.js
@@ -0,0 +1,16 @@
+//aspas duplas
+const texto1 = "Olá, mundo!";
+
+//aspas simples
+const tedto2 = 'Olá, mundo!';
+
+//string de numeros
+const StringDeNumeros = "56487989";
+
+//concatenação
+const citacao = "Meu nome é ";
+
+const meuNome = "Elias.";
+
+console.log(citacao + meuNome);
+
