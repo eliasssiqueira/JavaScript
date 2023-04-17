16043ad1fbf09199e6f525523148431d08781a33
Elias <eliassiqueira584@gmail.com>
2023-04-17 06:23:42 -0300

    Create type-number.js

---
+++ fundamentos/type-number.js
@@ -0,0 +1,12 @@
+const primeiroNumero = 5;
+const segundoNumero =10;
+
+const soma = primeiroNumero + segundoNumero;
+const subtracao = primeiroNumero - segundoNumero;
+const multiplicacao = primeiroNumero * segundoNumero;
+const divisao = primeiroNumero / segundoNumero;
+
+console.log(soma)
+console.log(subtracao)
+console.log(multiplicacao)
+console.log(divisao)
