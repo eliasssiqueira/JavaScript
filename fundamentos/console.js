049d2113c2d24535352bac271e00973c4063ebac
Elias <eliassiqueira584@gmail.com>
2023-04-18 06:31:22 -0300

    fundamentos: comparações e console

---
+++ fundamentos/console.js
@@ -0,0 +1,11 @@
+//log --> registro
+
+const minhaVar = 0;
+
+console.log(245);
+console.log('eu sou um texto');
+console.log(minhaVar);
+
+//tratamento de erro
+
+console.log("deu erro!");
--- fundamentos/conversoes.js
+++ fundamentos/conversoes.js
@@ -5,3 +5,17 @@ const numeroString = '456';
 //funcao Number() e String()
 
 const numero = Number('456');
+
+
+var respostaDeTudo = 42
+let idade = 29
+const pi = 3.14
+
+{
+    var respostaDeTudo = 3.14
+    let idade = 42
+    const pi = 29
+    console.log(respostaDeTudo, idade, pi)
+
+}
+console.log(respostaDeTudo, idade, pi)
