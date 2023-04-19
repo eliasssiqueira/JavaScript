823f71b3ee95c69b9e95bf62f0fba3343e2b543c
Elias <eliassiqueira584@gmail.com>
2023-04-19 20:17:09 -0300

    Update comparacoes.js

---
+++ fundamentos/comparacoes.js
@@ -0,0 +1,16 @@
+// == (comparação implicita) compara apenas o valor
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto)
+
+// === compara valor e o tipo de dado
+const numero1 = 5;
+const texto1 = "5";
+
+console.log(numero === texto)
+
+
+//typeof -> mostra o tipo de dado que esta armazenado na variavel.
+console.log(typeof numero);
+console.log(typeof texto);
