5d475f26c9fdb788965f90e8213e45745bc2f438
Elias <eliassiqueira584@gmail.com>
2023-04-19 21:07:33 -0300

    template string, ternario

---
+++ fundamentos/template-strings.js
@@ -0,0 +1,13 @@
+//const nome = 'elias';
+//const idade = '30';
+//const cidadeNascimento = 'Caxias do Sul';
+//
+//const apresentacao = `Meu nome é ${nome}, tenho ${idade} anos e nasci em ${cidadeNascimento}.`;
+//console.log(apresentacao)
+
+const nome = "Leo";
+const idade = 23;
+const bebidaMaior = "cerveja";
+const bebidaMenor = "suco";
+
+
---
+++ fundamentos/ternario.js
@@ -0,0 +1,17 @@
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+//exemplo com o if
+
+//  if(idadeCliente >= idadeMinima){//
+//    console.log('cerveja');
+
+//  }else {
+//    console.log('suco')
+//  }
+
+//exemplo com ternario
+
+
+             //condição                    //true     //false
+console.log(idadeCliente >= idadeMinima ? 'cerveja' : 'suco');
