ffb6372f26e1bc5d92448cd413598270939999c2
Elias <eliassiqueira584@gmail.com>
2023-04-19 21:08:31 -0300

    Update template-strings.js

--- fundamentos/template-strings.js
+++ fundamentos/template-strings.js
@@ -1,13 +1,10 @@
-//const nome = 'elias';
-//const idade = '30';
-//const cidadeNascimento = 'Caxias do Sul';
-//
-//const apresentacao = `Meu nome é ${nome}, tenho ${idade} anos e nasci em ${cidadeNascimento}.`;
-//console.log(apresentacao)
+const nome = 'elias';
+const idade = '30';
+const cidadeNascimento = 'Caxias do Sul';
 
-const nome = "Leo";
-const idade = 23;
-const bebidaMaior = "cerveja";
-const bebidaMenor = "suco";
+const apresentacao = `Meu nome é ${nome}, tenho ${idade} anos e nasci em ${cidadeNascimento}.`;
+
+console.log(apresentacao);
+
 
 
